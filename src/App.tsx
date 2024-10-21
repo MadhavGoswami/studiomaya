bd2b4de3b1516a6adf2d7f8e605772102742c3d7
Madhav <goswamimadhav16@gmail.com>
2024-10-21 23:18:08 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="heading1  w-full flex justify-center text-rose-500 ">
+       <h1>Hello,Let's Build Studio_Maya Website</h1>
+    </div>
+  )
+}
+
+export default App
