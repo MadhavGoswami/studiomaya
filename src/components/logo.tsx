718bb9ffae19d9471f0378d93599d3707205223e
Madhav <goswamimadhav16@gmail.com>
2025-05-05 20:35:35 +0530

    Fix image paths of logo for deployment

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo: React.FC = () => {
   return (
     <div className="text-white font-bold text-xl lg:text-2xl">
-      <img src="public/assets/ProjectImages/logowhite.png" alt="Logo" className="h-8 lg:h-10" />
+      <img src="assets/ProjectImages/logowhite.png" alt="Logo" className="h-8 lg:h-10" />
     </div>
   );
 };
